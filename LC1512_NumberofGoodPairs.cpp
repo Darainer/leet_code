955b73c71efc101b3efdafaba902dc92d98b731c
Darainer <ryanjmatthews@gmail.com>
2020-07-19 09:56:17 +0200

    fixed repeated variable declaration

--- LC1512_NumberofGoodPairs.cpp
+++ LC1512_NumberofGoodPairs.cpp
@@ -27,7 +27,7 @@ using namespace std;
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int i{0},j{0},count{0};
+        int count{0};
         for(int i = 0; i<nums.size()-1;i++){
             for(int j= i+1; j<nums.size();j++){
                 if(nums[i] == nums[j]){
