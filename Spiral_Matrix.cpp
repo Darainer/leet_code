fcf7adadd8275709f062c0a5f07f002f2b1acb2e
Darainer <ryanjmatthews@gmail.com>
2021-04-11 21:27:39 +0200

    complete, still have off by one boundary error

--- Spiral_Matrix.cpp
+++ Spiral_Matrix.cpp
@@ -36,69 +36,70 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
     if(matrix.size()==0){return {};}
-        
-        Direction direction{RIGHT};
-        int row_idx{0}, col_idx{0};
-        int boundary_top{0}, boundary_bottom{static_cast<int>(matrix.size()-1)};
-        int boundary_left{0}, boundary_right{static_cast<int>(matrix[0].size()-1)};
-        vector<int> result{};
+    
+    Direction direction{RIGHT};
+    int row_idx{0}, col_idx{0};
+    int boundary_top{0}, boundary_bottom{static_cast<int>(matrix.size()-1)};
+    int boundary_left{0}, boundary_right{static_cast<int>(matrix[0].size()-1)};
+    vector<int> result{};
+    result.push_back( matrix[row_idx][col_idx]);
 
-    while(boundary_bottom!= boundary_top && boundary_left != boundary_right){  //test that the bounds have not fully converged
-        result.push_back( matrix[row_idx][col_idx]);
+    while(!(boundary_bottom <= boundary_top && boundary_left >= boundary_right)){  //test that the bounds have not fully converged
         switch(direction){
-
         case RIGHT :
-            if(col_idx +1 < boundary_right){
+            if(col_idx +1 <= boundary_right){
                 col_idx++;
-
+                result.push_back( matrix[row_idx][col_idx]);
             }
             else{
                 direction = DOWN;
                 boundary_top +=1;
-            };
-            
-        case DOWN : 
-        {
-            if(row_idx+1 < boundary_bottom){
+            }
+            break;
+        case DOWN :
+            if(row_idx+1 <= boundary_bottom){
                 row_idx++;
+                result.push_back( matrix[row_idx][col_idx]);
             }
             else{
-                direction= LEFT;
+                direction = LEFT;
                 boundary_right -=1;  
             }
-        }
+            break;
         case LEFT :
-            if(col_idx -1 < boundary_left){
+            if(col_idx -1 >= boundary_left){
                 col_idx--;
+                result.push_back( matrix[row_idx][col_idx]);
             }
             else{
                 direction = UP;
                 boundary_bottom -=1;
             }
+            break;
         case UP :
-            if(row_idx -1 < boundary_top){
+            if(row_idx -1 >= boundary_top){
                 row_idx--;
+                result.push_back( matrix[row_idx][col_idx]); 
             }
             else{
                 direction = RIGHT;
                 boundary_left +=1;
             }
-        }
-
+            break;
+        } //switch direction
 }
+    return result;
+};  
 
-};
-
-
+};  //Solution
 
 int main(){
     std::vector<std::vector<int>> Input {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
     //std::vector<int> Output_ans {1,2,3,4,8,12,11,10,9,5,6,7};
-    std::vector<std::vector<int>> input{{3}, {2}};
-    print_utils::print_matrix(input);
+    print_utils::print_matrix(Input);
     Solution mysolution{};
-    std::vector<int> answer = mysolution.spiralOrder(input);
+    std::vector<int> answer = mysolution.spiralOrder(Input);
     print_utils::print_vector(answer);
 
     return 0;
-}
+};
