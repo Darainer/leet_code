0f28d945f32f3cffc28b406efb117773cdea00ba
Darainer <ryanjmatthews@gmail.com>
2022-10-23 12:04:05 +0200

    efficient solution to string operations

--- DefanginganIPAddress.cpp
+++ DefanginganIPAddress.cpp
@@ -5,7 +5,27 @@
 using namespace std; 
 using std::ostream;
 
+
 class Solution {
+public:
+    string defangIPaddr(string address) {
+        string answer;
+        answer.reserve(address.length() + 6); // 6 is constant for IP addresses
+        for(auto i=0; i< address.length();  ++i ){
+            if(address.at(i) == '.' ) {         // use .at(i) for better error handling
+                answer.append("[.]");  
+            }
+            else {
+                answer.push_back(address.at(i));
+            }
+        }
+        return address;  
+        }
+    };
+
+
+
+class Solution_old {
 public:
     string defangIPaddr(string address) {
 
@@ -19,10 +39,17 @@ public:
         }
     };
 
+
 int main() {
     Solution s;
-    string address = "10.1.1.1";
+    string address = "192.102.10.1";
     string result = s.defangIPaddr(address);
     cout << result << endl;
     return 0;
 }
+
+// learnings
+// push_back() is the method single chars, append() for strings or multiple chars
+// if the size of the string is known in advance, use reserve() to allocate the memory.
+// wont make much difference for small strings like in this case, but can be significant for large strings
+// making a new string is more efficient than replacing the old one in this case (see Solution_old)
